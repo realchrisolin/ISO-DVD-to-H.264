0701d0a88b6077e1cbaf171191d1c235c5995f58
Chris Olin <login@chrisolin.com>
2012-01-03 11:29:30 -0500

    updates!

--- convert.sh
+++ convert.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
+#Enabling debug mode so we can see what is happening.
 find . -iname "*.iso" | while read file
 do
 	#Print second field because results start with ./<dir>/...
@@ -9,5 +10,9 @@ do
          fi
 	mkdir /path/to/converted_movies/"$dir"
 	HandBrakeCLI -v --preset="High Profile" -i "$file" -o /path/to/converted_movies/"$dir"/"$filename".mp4 -f mp4 -O --main-feature -m -B 160,440 -s 1,2,3,4,5,6,7,8,9 < /dev/null 2> /path/to/converted_movies/"$dir"/log.txt
+	#Comment the next line to prevent this script from renaming converted .iso files
+	#to .osi. Copy/paste and execute the following comment with the # after this script
+	#is done running to restore .iso extension
+	#find . -iname "*.osi" | while read f; do echo mv \"$f\" \"${f/.osi/.iso}\"; done
 	mv "$file" "$dir2"/"$filename".osi 
 done
